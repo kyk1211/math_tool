5121ac2ce3b799c1d134529e7808a67fa4723de0
곽윤구 <advsdvs@gmail.com>
2021-08-04 22:35:35 +0900

    Add divide function

--- calculate.py
+++ calculate.py
@@ -1,9 +1,12 @@
 # 기본계산기
 def add(x, y):
-  return x + y
+    return x + y
 
 def substract(x, y):
-  return x - y
+    return x - y
 
 def multiply(x, y):
-  return x * y
+    return x * y
+
+def divide(x, y):
+    return x / y
