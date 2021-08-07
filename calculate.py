b272e0d379c2dae133c0af8f12e64e8c1d32f754
곽윤구 <advsdvs@gmail.com>
2021-08-07 15:59:28 +0900

    Add get_Sum_ver2 function

--- calculate.py
+++ calculate.py
@@ -14,5 +14,8 @@ def divide_new(x, y):
 def get_Median(x, y):
     return (x+y)/2
 
-def get_Sum_ver1(n):
-    return n(n+1)/2
+def get_Sum_ver2(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum + i
+    return sum
